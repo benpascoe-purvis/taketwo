e2c24c118e611b0204b36f29dc394dae558a71b0
Reuben Bishop <reubenjamesbishop@gmail.com>
2023-09-17 20:47:49 +0930

    Formatting

--- chrome-extension/popup/popup.js
+++ chrome-extension/popup/popup.js
@@ -1,72 +1,56 @@
 let extractProductInfoButton = document.querySelector("#extract-button");
 let productInfoList = document.querySelector("#info-list");
 
+let productTitle = document.querySelector("#product-title");
+let productColor = document.querySelector("#product-color");
+let productBrand = document.querySelector("#product-brand");
+let productCategory = document.querySelector("#product-category");
+let productImage = document.querySelector("#product-image");
+
 // Run extract functions on click
 extractProductInfoButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: getProductColor,
+    func: getProductDetails,
   });
 });
 
-function updatePopup(newTitle) {
-  let productTitle = document.querySelector("#product-title");
+function validateInput(input) {
+  if (input == null) {
+    return "Not available";
+  }
+
+  return input;
 }
 
-// // Listen for reponse
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  let productDetails = {
-    title: request.productTitle,
-    color: request.productColor,
-    brand: request.productBrand,
-    productCategory: request.productCategory,
-  };
+  productTitle.innerText = validateInput(request.productTitle);
+  productColor.innerText = validateInput(request.productColor);
+  productBrand.innerText = validateInput(request.productBrand);
+  productCategory.innerText = validateInput(request.productCategory);
 
-  if (productDetails == null) {
-    let li = document.createElement("li");
-    li.innerText = "Product details are NULL";
-    productInfoList.appendChild(li);
-  } else {
-    let originalProductImage = document.createElement("img");
-    originalProductImage.src = request.imageLink;
-    originalProductImage.width = 100;
-    productInfoList.appendChild(originalProductImage);
-
-    // List all product details passed in
-    for (const [key, value] of Object.entries(productDetails)) {
-      console.log(key, value);
-      let li = document.createElement("li");
-      li.innerText = key + ": " + value;
-      productInfoList.appendChild(li);
-    }
-  }
+  productImage.src = validateInput(request.productImageLink);
+  productImage.width = 100;
 });
 
-function getProductColor() {
-  //RegEx to parse emails fom HTML code
-  const productColor =
-    document.getElementsByClassName("color-name")[0].innerText;
-
-  const productTitle =
-    document.getElementsByClassName("product-title")[0].innerText;
-
-  const productBrand =
-    document.getElementsByClassName("product-brand")[0].innerText;
-
+function getProductDetails() {
+  const productColor = document.querySelector(".color-name").innerText;
+  const productTitle = document.querySelector(".product-title").innerText;
+  const productBrand = document.querySelector(".product-brand").innerText;
+  const productImageLink = document.querySelector(".product-image-frame").href;
   const productCategory = document
     .querySelector(".breadcrumbs")
     .querySelector("li:last-child")
     .textContent.trim();
 
-  const imageLink = document.querySelector(".product-image-frame").href;
-
-  //Send emails to popup
+  // alert(imageLink);
+  //Send to popup
   chrome.runtime.sendMessage({
     productColor,
     productTitle,
     productBrand,
     productCategory,
-    imageLink,
+    productImageLink,
   });
 }
