7d89697c3877080c1f48a19d8941ea237c8f4272
benpascoe-purvis <149132506+benpascoe-purvis@users.noreply.github.com>
2023-10-28 14:07:39 +1030

    Update scrapers.js

--- chrome-extension/popup/scrapers.js
+++ chrome-extension/popup/scrapers.js
@@ -1,14 +1,8 @@
 export function scrapeProductDetails_asos() {
   const productTitle = document.querySelector(".jcdpl").innerText;
-  const productCategory = document
-    .querySelector(".breadcrumbs")
-    .querySelector("li:last-child")
-    .textContent.trim();
-    
+
   //Send to popup
   chrome.runtime.sendMessage({
     productTitle,
-    productGender,
-    productCategory,
   });
 }
