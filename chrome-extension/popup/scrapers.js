4f3cf77cf9fb2d78d627f10d414773ab1e42639f
benpascoe-purvis <149132506+benpascoe-purvis@users.noreply.github.com>
2023-10-28 12:59:57 +1030

    Update scrapers.js

--- chrome-extension/popup/scrapers.js
+++ chrome-extension/popup/scrapers.js
@@ -1,24 +1,14 @@
-export function scrapeProductDetails_iconic() {
-  const productColor = document.querySelector(".color-name").innerText;
-  const productTitle = document.querySelector(".product-title").innerText;
-  const productBrand = document.querySelector(".product-brand").innerText;
-  const productImageLink = document.querySelector(".product-image-frame").href;
-  const productGender = document
-    .querySelector(".breadcrumbs")
-    .querySelector("li:first-child")
-    .textContent.trim();
+export function scrapeProductDetails_asos() {
+  const productTitle = document.querySelector(".jcdpl").innerText;
   const productCategory = document
     .querySelector(".breadcrumbs")
     .querySelector("li:last-child")
     .textContent.trim();
-
+    
   //Send to popup
   chrome.runtime.sendMessage({
-    productColor,
     productTitle,
-    productBrand,
     productGender,
     productCategory,
-    productImageLink,
   });
 }
